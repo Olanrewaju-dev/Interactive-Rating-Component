083b90fdc00a3803ff4cff11d3860b5a7972e784
Olanrewaju <olanrewaju20@gmail.com>
2022-03-31 15:17:43 +0100

    fixed the dynamic rating feedback after selection

--- script.js
+++ script.js
@@ -4,29 +4,41 @@ var submit = document.getElementById("btn");
 
 var thankUCard = document.getElementById("thankYou");
 
-function onClick() {
+function onSubmit() {
   ratingCard.classList.add("noDisplay");
   thankUCard.classList.remove("Display");
 }
 
-submit.addEventListener("click", onClick);
+submit.addEventListener("click", onSubmit);
 
-// var rating = document.getElementById("selected-rating");
+var rating = document.getElementById("selected-rating");
 
-// var rating1 = document.getElementById("rating-1");
-// var rating2 = document.getElementById("rating-2");
-// var rating3 = document.getElementById("rating-3");
-// var rating4 = document.getElementById("rating-4");
-// var rating5 = document.getElementById("rating-5");
+var rating1 = document.getElementById("rating-1");
+var rating2 = document.getElementById("rating-2");
+var rating3 = document.getElementById("rating-3");
+var rating4 = document.getElementById("rating-4");
+var rating5 = document.getElementById("rating-5");
 
-// function onClickRating() {
-//   switch (rating) {
-//     case 1:
-//       return = ;
-//       break;
-//     default:
-//       break;
-//   }
-// }
+rating1.addEventListener("click", () => {
+  return (rating.innerHTML = " 1 of 5 star rating");
+});
 
-// rating.addEventListener("click", onClickRating);
+rating2.addEventListener("click", () => {
+  return (rating.innerHTML = " 2 of 5 star rating");
+});
+
+rating3.addEventListener("click", () => {
+  return (rating.innerHTML = " 3 of 5 star rating");
+});
+
+rating3.addEventListener("click", () => {
+  return (rating.innerHTML = " 3 of 5 star rating");
+});
+
+rating4.addEventListener("click", () => {
+  return (rating.innerHTML = " 4 of 5 star rating");
+});
+
+rating5.addEventListener("click", () => {
+  return (rating.innerHTML = " 5 of 5 star rating!");
+});
